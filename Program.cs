ac90f83f8e4247b3f0150a39f990e7eae95185ba
Kleiton MQ <kleiton.x5@gmail.com>
2022-05-22 21:13:47 -0300

    concluido aula de leitura de arquivo externo

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+var path = Path.Combine(Environment.CurrentDirectory, "entrada", "usuarios-exportacao.csv");
+using var sr = new StreamReader(path);
+
+var cabecalho = sr.ReadLine()?.Split(','); //O ponto de interrogação é para evitar erro caso o split estaja null
+
+while(true)
+{
+    var registro = sr.ReadLine()?.Split(',');
+    if(registro == null) break;
+    for (int i = 0; i < registro.Length; i++)
+    {
+        WriteLine($"{cabecalho?[i]}:{registro[i]}");        
+    }
+    WriteLine("--------------");
+}
+
+WriteLine("precione enter para finalizar");
+ReadLine();
